b9d4204e56a8482db67311f4910d8face38bea64
Vivek Vishwakarma <vivekvish0134@gmail.com>
2024-06-06 13:38:16 +0530

    Add files via upload

---
+++ NUMBER-GUESSING-GAME.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
+
+using namespace std;
+
+int main() {
+    // Seed the random number generator
+    srand(static_cast<unsigned int>(time(0)));
+
+    // Generate a random number between 1 and 100
+    int randomNumber = std::rand() % 100 + 1;
+    int guess = 0;
+    int attempts = 0;
+
+    cout << "Welcome to the Number Guessing Game!" << endl;
+    cout << "I have selected a number between 1 and 100." << endl;
+    cout << "Can you guess what it is?" << endl;
+
+    // Loop until the user guesses the correct number
+    while (guess != randomNumber) {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess < randomNumber) {
+            cout << "Too low! Try again." << endl;
+        } else if (guess > randomNumber) {
+            cout << "Too high! Try again." << endl;
+        } else {
+            cout << "Congratulations! You guessed the correct number!" << endl;
+            cout << "It took you " << attempts << " attempts." << endl;
+        }
+    }
+
+    return 0;
+}
